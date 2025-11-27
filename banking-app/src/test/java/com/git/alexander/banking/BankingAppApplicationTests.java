1587f8ec06bb2d6e8624d09f5a1318be445e9385
alexander1914 <alexander.oliveira99@gmail.com>
2025-11-27 16:45:10 +0000

    [Set up Project] - I created new project bankingApp today

---
+++ banking-app/src/test/java/com/git/alexander/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.alexander.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
