b693a470fa9ce7fd41dc5c7843444f2df6165ef0
alexander1914 <alexander.oliveira99@gmail.com>
2025-12-17 22:15:36 +0000

    [Transaction History] - I had been learning like to create a transaction history today

---
+++ banking-app/src/main/java/com/git/alexander/banking/entity/Transaction.java
@@ -0,0 +1,26 @@
+package com.git.alexander.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long accountId;
+    private double amount;
+    private String transactionType;/// Deposit, Withdraw and Transfer
+    private LocalDateTime timestamp;
+}
